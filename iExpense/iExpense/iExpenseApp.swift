c02f8691b334d79be6507d9128d04bfb94b5eeb2
Jacob087 <162118004+Jacob087@users.noreply.github.com>
2024-03-06 14:38:22 +0000

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tesco Labs on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
